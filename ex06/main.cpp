239fcc4e8826ff8edfd77fe86c86d002506b3419
Mi Zhou <mzhou@student.42.fr>
2025-11-24 14:10:27 +0100

    update(*):makefile remove debug flag, improve error handeling

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -1,10 +1,12 @@
 #include "Harl.hpp"
 #include <iostream>
+#include <cstdlib>
+
 int main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		std::cerr << "say something Harl\n";
+		std::cerr << "say one thing and only one thing Harl\n";
 		return (EXIT_FAILURE);
 	}
 	Harl	oneHarl;
